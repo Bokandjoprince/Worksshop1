1843e6c07a57bdda00696ef5aea9e9a25b1eb3ae
HP <HP@DESKTOP-PRINCE>
2022-10-03 15:57:27 +0100

    first commit

---
+++ Worksshop1/src/Workshop.java
@@ -0,0 +1,12 @@
+public class Workshop { 
+/**
+ * @param args
+ */
+public static void main(String[] args) { 
+ Personne unePersonne = new Personne(); 
+ unePersonne.nom = "Diet"; 
+ unePersonne.prenom = "Jean-Aymeric"; 
+ unePersonne.direBonjour();
+ System.out.println(unePersonne.nom); 
+ } 
+} 
